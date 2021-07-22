b455a15241789c64a8ec2e02714fdd37530a3a37
Frederico Reis <reis@devgrid.co.uk>
2021-07-22 08:23:11 -0300

    chore: first commit

---
+++ src/Components/Test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+interface Props {
+  title: string
+}
+
+export const Teste = () => {
+  return (
+    <View>
+      <Text>Loading</Text>
+    </View>
+  );
+}
+
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { } from 'react-native';
+
+import { Container, Title } from './styles';
+
+export const Dashboard = () => {
+  return (
+    <Container>
+      <Title>Dashboard</Title>
+    </Container>
+  );
+}
